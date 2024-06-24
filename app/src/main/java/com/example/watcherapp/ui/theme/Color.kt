bcb6cf4a8dcf7d9b22f2f15f757bbe84984429b4
gabisponciano <gabriela.ponciano@alu.ufc.br>
2024-06-24 10:11:14 -0300

    Cores

--- app/src/main/java/com/example/watcherapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/watcherapp/ui/theme/Color.kt
@@ -2,6 +2,12 @@ package com.example.watcherapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val background = Color(0xFF252323)
+val componentsBackground = Color(0xFF383838)
+val redComponent = Color(0xFFbb0b0b)
+val greenComponent = Color(0xFF006600)
+
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
